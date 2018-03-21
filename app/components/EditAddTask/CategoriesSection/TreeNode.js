d1bf1937c65efded781f90d9f7aec670765ce477
Denis Sidarenka <dongor7@gmail.com>
2018-03-21 14:41:22 +0300

    Initial commit

---
+++ app/components/EditAddTask/CategoriesSection/TreeNode.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default class TreeNode extends React.PureComponent {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      children: this.props.children,
+      child: this.props.node,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  render() {
+
+    let nodes = null;
+
+    if(this.props.children){
+      nodes = this.state.children.map(child => {
+
+        //this.state.child = child;
+
+        if(child.visible)
+          return <TreeNode
+            key={child.id}
+            node={child}
+            children={child.children}
+            onChangeVisible={this.props.onChangeVisible}/>;
+      })
+    }
+
+    return (
+      <li>
+        <span onClick={() => this.toggle(this.state.child)}>V</span>
+        <span>{this.state.child.name}</span>
+        <ul>{nodes}</ul>
+      </li>
+    );
+  }
+
+  toggle(node) {
+
+    /*let child = this.state.child;
+    child.id = "13243424";
+    child.visible = !child.visible;
+
+    this.setState({child});*/
+    //console.log(child);
+
+    this.props.onChangeVisible(node);
+  }
+}
+
